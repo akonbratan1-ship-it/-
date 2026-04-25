4b24a391690c1dbebc504434c46aadd292d71d2e
akonbratan1-ship-it <akonbratan1@gmail.com>
2026-04-26 03:12:08 +0600

    Create data.js

---
+++ src/data/data.js
@@ -0,0 +1,14 @@
+export const features = [
+  {
+    title: 'HD Музыка',
+    desc: 'Жоғары сапалы тректер',
+  },
+  {
+    title: 'Playlist',
+    desc: 'Жеке плейлист жасау',
+  },
+  {
+    title: 'Live Concerts',
+    desc: 'Тікелей эфир концерттері',
+  },
+]
